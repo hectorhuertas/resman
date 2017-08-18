c5f4f136773e4014c4a649a187625df1f5534d3f
Hector Huertas <hectorhuertas@gmail.com>
2017-08-19 00:09:56 +0100

    Detect clean and dirty repos

--- src/main.go
+++ src/main.go
@@ -5,67 +5,70 @@ import "fmt"
 // import "log"
 import "os"
 import "path/filepath"
-// import "os/exec"
+import "os/exec"
 
 func main() {
-  CODEPATH:=os.Getenv("HOME")+"/xdev"
-  fmt.Printf("CODEPATH: %s\n", CODEPATH)
-  // fmt.Println("jsalkdfjaslf")
-  p1 := "/usr"
-  p2 := "/Users/hh/xdev/go/resource-manager"
-  // // binary, lookErr := exec.LookPath("git")
-  // // if lookErr != nil {
-  // //     log.Fatal(lookErr)
-  // // }
-  // // cmdX := binary
-  // // args := []string{"status",""}
-  // // output, err := exec.Command(cmdX, args...).Output()
-  // cmd := exec.Command("/usr/bin/git", "status")
-  // output, err := cmd.Output()
-  // // fmt.Println(cmdX, output, "\n", args)
-  // fmt.Println(cmd, output, err)
-  // out, err := exec.Command("git", "status", "--porcelsain").Output()
-  // if err != nil {
-  //  log.Fatal(err)
-  // }
-  // fmt.Printf("The date is %s\n", out)
-  // out, err := exec.Command("git", "status", "--porcelsain").Output()
-  // if err != nil {
-  // log.Fatal(err)
-  // }
-  // fmt.Printf("%s",bob.Name(),"\nJANDER\n", err)
-  // fmt.Printf("%s", bob.Name())
-  fmt.Printf("TEST: %s: %t\n", p1, isGitRepo(p1))
-  fmt.Printf("TEST: %s: %t\n", p2, isGitRepo(p2))
-  // fmt.Printf("%t\n", isGitRepo("/usr"))
-  reposPaths := []string{}
-  filepath.Walk(CODEPATH, getGitRepos(&reposPaths))
-  // kak := make([]string, 0)
-  // fmt.Printf("One: %v\n", kak)
-  // fmt.Printf("One: %v\n", reposPaths)
-  for _, repoPath := range reposPaths {
-        // fmt.Printf("Name: %s Age: %d\n", dog.Name, dog.Age)
-        fmt.Printf("Path: %s\n", repoPath)
+	CODEPATH := os.Getenv("HOME") + "/xdev"
+	fmt.Printf("CODEPATH: %s\n", CODEPATH)
+	// // cmdX := binary
+	// // args := []string{"status",""}
+	// // output, err := exec.Command(cmdX, args...).Output()
+	// cmd := exec.Command("/usr/bin/git", "status")
+	// output, err := cmd.Output()
+	// // fmt.Println(cmdX, output, "\n", args)
+	reposPaths := []string{}
+	filepath.Walk(CODEPATH, getGitRepos(&reposPaths))
 
-        // fmt.Println("")
-    }
-  // bob = append(bob, "jander")
-  // fmt.Printf("Two: %v\n", bob)
+	bob := unmodifiedRepos(reposPaths)
+	fmt.Printf("Clean: %v\n", bob)
+
+	jar := modifiedRepos(reposPaths)
+	fmt.Printf("Dirty: %v\n", jar)
 }
 
-
-func getGitRepos(reposPaths *[]string) filepath.WalkFunc{
-  return func (path string, f os.FileInfo, err error) error {
-    if isGitRepo(path) {
-      *reposPaths = append(*reposPaths, path)
-      return filepath.SkipDir
-    }
-    return nil
-  }
+func getGitRepos(reposPaths *[]string) filepath.WalkFunc {
+	return func(path string, f os.FileInfo, err error) error {
+		// if path == "/Users/hh/xdev/repositive" {
+		// 	return filepath.SkipDir
+		// }
+		if isGitRepo(path) {
+			*reposPaths = append(*reposPaths, path)
+			return filepath.SkipDir
+		}
+		return nil
+	}
 }
 
 func isGitRepo(path string) bool {
-  gitPath := path + "/.git"
-  _, err := os.Stat(gitPath)
-  return err == nil
+	gitPath := path + "/.git"
+	_, err := os.Stat(gitPath)
+	return err == nil
+}
+
+func modifiedRepos(reposPaths []string) []string {
+	return filterReposIfStatus(reposPaths, isClean)
+}
+
+func unmodifiedRepos(reposPaths []string) []string {
+	return filterReposIfStatus(reposPaths, isDirty)
+}
+
+func filterReposIfStatus(reposPaths []string, f func(string)bool) []string {
+	modifiedRepos := []string{}
+	for _, repoPath := range reposPaths {
+		out, _ := exec.Command("git", "-C", repoPath, "status", "--porcelain").Output()
+		if f(string(out)) {
+			modifiedRepos = append(modifiedRepos, repoPath)
+		}
+	}
+	return modifiedRepos
+
+}
+
+func isDirty(s string) bool  {
+	return s == ""
+}
+
+func isClean(s string) bool  {
+	return s != ""
 }
