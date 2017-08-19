8df6f562bde16e73a91c4a349a13d84cc30708bd
Hector Huertas <hectorhuertas@gmail.com>
2017-08-19 16:18:57 +0100

    Extract functionality to a package

---
+++ local/local.go
@@ -0,0 +1,58 @@
+package local
+
+// import "fmt"
+
+// import "log"
+import "os"
+import "path/filepath"
+import "os/exec"
+
+
+func GetGitRepos(reposPaths *[]string) filepath.WalkFunc {
+	return func(path string, f os.FileInfo, err error) error {
+		// if path == "/Users/hh/xdev/repositive" {
+		// 	return filepath.SkipDir
+		// }
+		if isGitRepo(path) {
+			*reposPaths = append(*reposPaths, path)
+			return filepath.SkipDir
+		}
+		return nil
+	}
+}
+
+func isGitRepo(path string) bool {
+	gitPath := path + "/.git"
+	_, err := os.Stat(gitPath)
+	return err == nil
+}
+
+func ModifiedRepos(reposPaths []string) []string {
+	return filterReposIfStatus(reposPaths, isClean)
+}
+
+func UnmodifiedRepos(reposPaths []string) []string {
+	return filterReposIfStatus(reposPaths, isDirty)
+}
+
+func filterReposIfStatus(reposPaths []string, f func(string)bool) []string {
+	//TODO: This is only checking either the current or the master branch. What
+	// should be the proper check?
+	modifiedRepos := []string{}
+	for _, repoPath := range reposPaths {
+		out, _ := exec.Command("git", "-C", repoPath, "status", "--porcelain").Output()
+		if f(string(out)) {
+			modifiedRepos = append(modifiedRepos, repoPath)
+		}
+	}
+	return modifiedRepos
+
+}
+
+func isDirty(s string) bool  {
+	return s == ""
+}
+
+func isClean(s string) bool  {
+	return s != ""
+}
--- one/one.go
+++ one/one.go
@@ -5,7 +5,8 @@ import "fmt"
 // import "log"
 import "os"
 import "path/filepath"
-import "os/exec"
+// import "os/exec"
+import  "resource-manager/local"
 
 func main() {
 	CODEPATH := os.Getenv("HOME") + "/xdev"
@@ -17,60 +18,11 @@ func main() {
 	// output, err := cmd.Output()
 	// // fmt.Println(cmdX, output, "\n", args)
 	reposPaths := []string{}
-	filepath.Walk(CODEPATH, getGitRepos(&reposPaths))
+	filepath.Walk(CODEPATH, local.GetGitRepos(&reposPaths))
 
-	bob := unmodifiedRepos(reposPaths)
+	bob := local.UnmodifiedRepos(reposPaths)
 	fmt.Printf("Clean: %v\n", bob)
 
-	jar := modifiedRepos(reposPaths)
+	jar := local.ModifiedRepos(reposPaths)
 	fmt.Printf("Dirty: %v\n", jar)
-}
-
-func getGitRepos(reposPaths *[]string) filepath.WalkFunc {
-	return func(path string, f os.FileInfo, err error) error {
-		// if path == "/Users/hh/xdev/repositive" {
-		// 	return filepath.SkipDir
-		// }
-		if isGitRepo(path) {
-			*reposPaths = append(*reposPaths, path)
-			return filepath.SkipDir
-		}
-		return nil
-	}
-}
-
-func isGitRepo(path string) bool {
-	gitPath := path + "/.git"
-	_, err := os.Stat(gitPath)
-	return err == nil
-}
-
-func modifiedRepos(reposPaths []string) []string {
-	return filterReposIfStatus(reposPaths, isClean)
-}
-
-func unmodifiedRepos(reposPaths []string) []string {
-	return filterReposIfStatus(reposPaths, isDirty)
-}
-
-func filterReposIfStatus(reposPaths []string, f func(string)bool) []string {
-	//TODO: This is only checking either the current or the master branch. What
-	// should be the proper check?
-	modifiedRepos := []string{}
-	for _, repoPath := range reposPaths {
-		out, _ := exec.Command("git", "-C", repoPath, "status", "--porcelain").Output()
-		if f(string(out)) {
-			modifiedRepos = append(modifiedRepos, repoPath)
-		}
-	}
-	return modifiedRepos
-
-}
-
-func isDirty(s string) bool  {
-	return s == ""
-}
-
-func isClean(s string) bool  {
-	return s != ""
 }
