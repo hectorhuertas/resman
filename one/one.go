a737d626baec221e5c34dab443db3abac56f4f8d
Hector Huertas <hectorhuertas@gmail.com>
2017-08-20 16:45:39 +0100

    Clean main

--- one/one.go
+++ one/one.go
@@ -7,16 +7,14 @@ import "os"
 import  "resource-manager/gitLocal"
 
 func main() {
-	CODEPATH := os.Getenv("HOME") + "/xdev"
-	fmt.Printf("CODEPATH: %s\n", CODEPATH)
-	// // cmdX := binary
-	// // args := []string{"status",""}
-	// // output, err := exec.Command(cmdX, args...).Output()
-	// cmd := exec.Command("/usr/bin/git", "status")
-	// output, err := cmd.Output()
-	// // fmt.Println(cmdX, output, "\n", args)
-	fmt.Printf("Clean: %v\n", gitLocal.GetUnmodifiedRepos(CODEPATH))
-	fmt.Printf("Dirty: %v\n", gitLocal.GetModifiedRepos(CODEPATH))
+	scan()
+}
 
-	fmt.Printf("All: %v\n", gitLocal.GetRepos(CODEPATH))
+func scan()  {
+	CODEPATH := os.Getenv("HOME") + "/xdev" // Might file for / dir
+	fmt.Printf("CODEPATH: %s\n", CODEPATH)
+
+	// fmt.Printf("All: %v\n", gitLocal.GetRepos(CODEPATH))
+	// fmt.Printf("Clean: %v\n", gitLocal.GetUnmodifiedRepos(CODEPATH))
+	fmt.Printf("Dirty: %v\n", gitLocal.GetModifiedRepos(CODEPATH))
 }
