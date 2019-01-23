27a7330e2664c304560a69345f400baaa9c26bc5
Hector Huertas <hectorhuertas@gmail.com>
2019-01-23 10:23:33 +0100

    Setup command folder

---
+++ cmd/helloCmd.go
@@ -0,0 +1,7 @@
+package cmd
+
+import "github.com/hectorhuertas/resource-manager/hello"
+
+func HelloCmd() {
+	hello.Hola()
+}
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import "fmt"
+import "github.com/hectorhuertas/resource-manager/cmd"
 
 func main() {
 	fmt.Println("Start")
+	cmd.HelloCmd()
 }
