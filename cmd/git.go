2f93d3914a196cd52c13d7c2a38b4131b2886816
Hector Huertas <hectorhuertas@gmail.com>
2019-02-01 19:36:22 +0100

    Implement basic scan with root and excludes

--- cmd/git.go
+++ cmd/git.go
@@ -10,7 +10,7 @@ import (
 func init() {
 	ResmanCmd.AddCommand(gitCmd)
 	gitCmd.Flags().StringP("root", "r", "/Users/hh/xdev", "root path to begin repository lookup")
-	gitCmd.Flags().StringP("exclude", "e", "/Users/hh/xdev/go", "exclude a path from the repository lookup")
+	gitCmd.Flags().StringArrayP("exclude", "e", []string{}, "exclude a path from the repository lookup")
 }
 
 var gitCmd = &cobra.Command{
@@ -20,10 +20,9 @@ var gitCmd = &cobra.Command{
 	Args:      cobra.OnlyValidArgs,
 	ValidArgs: []string{"git", "two"},
 	Run: func(cmd *cobra.Command, args []string) {
-		//fmt.Println(strings.Join(gitLocal.GetRepos("/Users/hh/xdev"), "\n"))
-		r := cmd.Flag("root").Value.String()
-		e := cmd.Flag("exclude").Value.String()
-		fmt.Print(git.MockGet())
-		git.Get(r, e)
+		root, _ := cmd.Flags().GetString("root")
+		exclude, _ := cmd.Flags().GetStringArray("exclude")
+		out := git.Scan(root, exclude)
+		fmt.Println(out)
 	},
 }
--- git/git.go
+++ git/git.go
@@ -1,18 +1,22 @@
 package git
 
-import "github.com/hectorhuertas/resman/git/github"
 import (
 	"fmt"
+	"os"
+	"path"
+	"path/filepath"
 	"strings"
+
+	"github.com/hectorhuertas/resman/git/github"
 )
 
 type git struct {
-	Name      string
+	name      string
 	localPath string
 }
 
 func (g git) String() string {
-	return fmt.Sprintf("%s", g.Name)
+	return fmt.Sprintf("%s", g.name)
 }
 
 type gitList []git
@@ -20,30 +24,44 @@ type gitList []git
 func (gs gitList) String() string {
 	var ret strings.Builder
 	for _, g := range gs {
-		ret.WriteString(g.Name)
+		ret.WriteString(g.name)
 		ret.WriteString("\n")
 	}
 	return ret.String()
 }
 
-func MockGet() gitList {
-	var gits []git
-	gits = append(gits, git{Name: "resman", localPath: "/Users/hh/xdev/resman"})
-	gits = append(gits, git{Name: "exercism", localPath: "/Users/hh/xdev/exercism"})
-	return gits
-}
+func Scan(root string, exclude []string) gitList {
+	paths, _ := findGitDirs(root, exclude)
 
-func Get(root string, exclude string) gitList {
-	fmt.Println(root)
-	fmt.Println(exclude)
 	u := github.User{"Pepe"}
 	fmt.Println(github.Repos(u))
 	o := github.Org{"uw-labs"}
 	fmt.Println(github.Repos(o))
 
 	var gits []git
-	gits = append(gits, git{Name: "resman", localPath: "/Users/hh/xdev/resman"})
-	gits = append(gits, git{Name: "exercism", localPath: "/Users/hh/xdev/exercism"})
+	for _, p := range paths {
+		gits = append(gits, git{name: path.Base(p), localPath: p})
+
+	}
 	return gits
 
+}
+
+func findGitDirs(root string, exclude []string) (ret []string, err error) {
+	filepath.Walk(root, func(path string, f os.FileInfo, err error) error {
+		for _, e := range exclude {
+			if path == e {
+				return filepath.SkipDir
+			}
+		}
+
+		gitPath := path + "/.git"
+		_, err = os.Stat(gitPath)
+		if err == nil {
+			ret = append(ret, path)
+			return filepath.SkipDir
+		}
+		return nil
+	})
+	return ret, nil
 }
