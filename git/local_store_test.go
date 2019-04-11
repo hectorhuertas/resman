f7e407846330d59731ef4cb6e4b928ed9ba95e7a
Hector Huertas <hectorhuertas@gmail.com>
2019-04-11 09:59:05 +0200

    Clarify test

--- git/local_store_test.go
+++ git/local_store_test.go
@@ -8,12 +8,12 @@ import (
 )
 
 func testLocalStorePort(ls git.LocalStore, t *testing.T) {
-	t.Run("add, get and delete", func(t *testing.T) {
+	t.Run("single add, get and delete", func(t *testing.T) {
 		// Given
-		l := git.Local{Location: "somewhere"}
+		newLocal := git.Local{Location: "somewhere"}
 
 		// When
-		err := ls.Add(l)
+		err := ls.Add(newLocal)
 
 		// Then
 		if err != nil {
@@ -21,21 +21,51 @@ func testLocalStorePort(ls git.LocalStore, t *testing.T) {
 		}
 
 		// When
-		got, err := ls.Get(l.Location)
+		got, err := ls.Get(newLocal.Location)
 
 		// Then
 		if err != nil {
 			t.Error("cannot get local")
 		}
-		if got != l {
-			t.Errorf("got wrong local: %v, expected: %v", got, l)
+		if got != newLocal {
+			t.Errorf("got wrong local: %v, expected: %v", got, newLocal)
 		}
 
 		// When
 		ls.Delete(got.Location)
 
 		// Then
-		_, err = ls.Get(l.Location)
+		_, err = ls.Get(newLocal.Location)
+		if err == nil {
+			t.Error("cannot delete local")
+		}
+
+		//fmt.Printf("got: %+v\n", got)
+		//fmt.Printf("want: %+v\n", want)
+
+	})
+	t.Run("multiple add, get and delete", func(t *testing.T) {
+		// Given
+		newLocal1 := git.Local{Location: "somewhere"}
+		ls.Add(newLocal1)
+		newLocal2 := git.Local{Location: "here"}
+		ls.Add(newLocal2)
+		newLocal3 := git.Local{Location: "there"}
+		ls.Add(newLocal3)
+
+		// When
+		got, _ := ls.Get(newLocal2.Location)
+
+		// Then
+		if got != newLocal2 {
+			t.Errorf("got wrong local: %v, expected: %v", got, newLocal2)
+		}
+
+		// When
+		ls.Delete(newLocal3.Location)
+
+		// Then
+		_, err := ls.Get(newLocal3.Location)
 		if err == nil {
 			t.Error("cannot delete local")
 		}
